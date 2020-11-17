c306d8a264e2729195ed36f2ece3f848a7efcf70
will <whyireland@protonmail.com>
2020-11-17 21:03:49 +0000

    create discord selfbot

--- auto-status.py
+++ auto-status.py
@@ -1,8 +1,18 @@
 import discord
 import argparse
 
+# Parse args
 parser = argparse.ArgumentParser()
 parser.add_argument("token")
 parser.add_argument("-t", "--time", help="Time between status change (sec)", type=int)
 args = parser.parse_args()
 
+# Create discord client
+dClient = discord.Client()
+
+@dClient.event
+async def on_ready():
+    print(f"logged in as {dClient.user}!")
+    
+
+dClient.run(args.token, bot=False)
